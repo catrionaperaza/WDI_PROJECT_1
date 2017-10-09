ca3a92a47353febe01ce26987a9992018603d233
odwyercatriona <odwyercatriona@gmail.com>
2017-10-09 11:19:09 +0100

    Saving plans

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
-console.log('loaded');
+window.onload = init;
+
+function init(){
+  Grab all JQuery elements here and call functions, examples:
+  $squares     = $('li');
+  $button.on('click', reset);
+}
+
+actionAnswer etc lis .hide()
+
+example of how you can hide a button and save it in a const to be referred to again within this function
+$('li').on('click', function() {
+  const $element = $(this);
+  $element.hide('slow', 'swing', () => {
+    setTimeout(function(){
+      $element.show();
+    }, 1000);
+  });
+});
+
+var myObj (animalValues) = {
+  key: value
+  dog: img
+  cat: img
+};
 
 const
 
