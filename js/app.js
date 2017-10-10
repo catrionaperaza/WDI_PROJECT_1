fbc0e75ba0939333a828786cc72a1436e7da4d82
odwyercatriona <odwyercatriona@gmail.com>
2017-10-10 23:52:09 +0100

    save

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 window.onload = init;
 
 const animals = ['Dog','Cat','Rabbit','Horse','Fish','Bird']; //add in gifs here as bonus
-const actualAnimalAnswer = [];
+let actualAnimalAnswer = [];
 // const actualActionAnswer = [];
-const userAnimalAnswer = []; //push id from animal buttons in here
+let userAnimalAnswer = []; //push id from animal buttons in here
 // const userActionAnswer = []; //push id from action buttons in here
 let $level = null;
 let $windows = null;
@@ -41,13 +41,13 @@ function init(){
   $title = $('.title');
   $nextChoice = $('.nextChoice');
   // $actionAnswer.hide();
-  $playAgain.hide();
   // $playWithMe.hide();
+  $playAgain.hide();
   $answerInstructions.hide();
   $windows.on('click', randomAnimal);
 }
 
-function randomAnimal() {
+function randomAnimal(e) {
   $title.hide();
   const animalRandom = animals[Math.floor(animals.length * Math.random())];
   actualAnimalAnswer.push(animalRandom);
@@ -61,11 +61,12 @@ function randomAnimal() {
   }, 1000);
   setTimeout(function () {
     $(newP).remove();
-  }, 1000);
+  }, 2000);
   console.log(animalRandom);
   if (animals <= 0) {
+    setTimeout($assignNames,2000);
     $message.hide();
-    getAnswer();
+    $answerInstructions.show();
   }
 }
 
@@ -83,64 +84,92 @@ function randomAnimal() {
 // }
 
 function $assignNames() {
+  console.log('Got to this point');
   $($windows).off('click');
   $dog.html('Dog');
-  $dog.on('click', push);
+  // $dog.on('click', push);
   $cat.html('Cat');
-  $cat.on('click', push);
+  // $cat.on('click', push);
   $fish.html('Fish');
-  $fish.on('click', push);
+  // $fish.on('click', push);
   $bird.html('Bird');
-  $bird.on('click', push);
+  // $bird.on('click', push);
   $horse.html('Horse');
-  $horse.on('click', push);
+  // $horse.on('click', push);
   $rabbit.html('Rabbit');
-  $rabbit.on('click', push);
+  // $rabbit.on('click', push);
+  checkMatch();
 }
 
-// function(){
-//     //wait for animation to finish before removing classes
-//     window.setTimeout( function(){
-//         element.removeClass('animated ' + animation);
-//     }, 2000);
 
-function getAnswer() {
-  console.log('Got to this point');
-  $answerInstructions.show();
-  setTimeout(function () {
-    $($assignNames()).fadeIn();
-  }, 1000);
-}
 
-function push(e) {
-  userAnimalAnswer.push(e.target.id);
-  console.log(e.target.id);
-  if (userAnimalAnswer.length === 6) {
-    winner();
-  }
-}
+// function push(e) {
+//   userAnimalAnswer.push(e.target.id);
+//   console.log(e.target.id);
+//   if (userAnimalAnswer.length === 6) {
+//     winner();
+//   }
+// }
 
-function winner() {
-  $answerInstructions.hide();
-  $level.hide();
-  $windows.hide();
-  $nextChoice.hide();
-  $title.hide();
-  $body.removeClass('hide-bg');
+// function winner() {
+//   $answerInstructions.hide();
+//   $level.hide();
+//   $windows.hide();
+//   $nextChoice.hide();
+//   $title.hide();
+//   $body.removeClass('hide-bg');
 
-  //win logic not working
-  //text staying at top of box
+//win logic not working
+//text staying at top of box
 
-  console.log(actualAnimalAnswer);
-  console.log(userAnimalAnswer);
-  if (userAnimalAnswer === actualAnimalAnswer) {
-    $message.html('Well Done! You would make a great pet owner!');
+let userClicks = 0;
+
+
+function checkMatch() {
+  $windows.one('click', function(e) {
+    $title.hide();
     $message.show();
-    console.log('this work');
-    if (userAnimalAnswer !== actualAnimalAnswer){
-      $message.html('Unlucky! I\'m sure you would still make a great pet owner!');
-      $message.show();
-      console.log('this too');
+    const $clicked = $(e.target).attr('id');
+    if ($clicked === actualAnimalAnswer[userClicks]) {
+      console.log('correct');
+      $message.text('correct!');
+      userAnimalAnswer.push($clicked);
+      userClicks++;
+      if(userAnimalAnswer.length === 6) {
+        console.log('game over');
+        $answerInstructions.hide();
+        $nextChoice.hide();
+        $windows.hide();
+        $level.hide();
+        $body.removeClass('hide-bg');
+        $message.html('Well Done! You got them all correct! You would make a great pet owner!');
+        $playAgain.show();
+      }
+    } else {
+      console.log('wrong');
+      $answerInstructions.hide();
+      $nextChoice.hide();
+      $windows.hide();
+      $level.hide();
+      $body.removeClass('hide-bg');
+      $message.text('Uh oh! Wrong answer but nice try! Do you want to play again?');
+      $playAgain.show();
     }
-  }
+  });
+  $playAgain.one('click', $playGameAgain);
+}
+//try to get the correct button to pulse every time answer is correct 
+function $playGameAgain() {
+  const clear = $windows.text;
+  $(clear).remove();
+  $title.show();
+  $message.hide();
+  actualAnimalAnswer = [];
+  userAnimalAnswer = [];
+  animals.push['Dog','Cat','Rabbit','Horse','Fish','Bird'];
+  $windows.show();
+  $level.show();
+  $body.addClass('hide-bg');
+  $nextChoice.show();
+  init();
 }
