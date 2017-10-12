05c807ba3aed0ddd62e3177ac15d0bf0f6efb875
odwyercatriona <odwyercatriona@gmail.com>
2017-10-12 18:33:26 +0100

    save this version

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ let userEmotionAnswer = [];
 let $level = null;
 let $windows = null;
 let $playAgain = null;
-let $answerInstructions = null;
 let $welcomeMessage = null;
 let $gameOutcome = null;
 let $main = null;
@@ -22,7 +21,6 @@ function init(){
   $windows = $('.windows');
   $welcomeMessage = $('.welcomeMessage');
   $playAgain = $('.playAgain');
-  $answerInstructions = $('.answerInstructions');
   $main = $('main');
   $correct = $('.correct');
   $gameOutcome = $('.gameOutcome');
@@ -31,10 +29,10 @@ function init(){
   currentLevel = 1;
   $level.html(`Level ${currentLevel}`);
   $playLevel2.hide();
-  $playAgain.show(); //change back to hide
+  $playAgain.hide();
   $playAgain.html('Play Again?');
   $windows.removeClass('show-bg');
-  $answerInstructions.hide();
+  $welcomeMessage.show();
   $correct.hide();
   $level2windows.hide();
   assignClick();
@@ -69,9 +67,8 @@ function buildLevel(emotions, event) {
   }, 1000);
   if (emotions <= 0) {
     setTimeout($changeNames,1000);
-    $answerInstructions.show();
+    $welcomeMessage.html('Can you click on the emotions in the order you saw them?');
     $windows.off('click');
-    $welcomeMessage.hide();
   }
 }
 
@@ -89,8 +86,8 @@ function $changeNames() {
 let userClicks = 0;
 
 function checkMatch() {
-  $windows.one('click', function() {
-    const $clicked = $(this).attr('id');
+  $windows.one('click', function(e) {
+    const $clicked = $(event.target).attr('id');
     if ($clicked === actualEmotionAnswer[userClicks]) {
       $correct.show();
       setTimeout(function () {
@@ -100,83 +97,86 @@ function checkMatch() {
       userClicks++;
 
       if (currentLevel === 1 && userEmotionAnswer.length === 4) {
+        $correct.hide();
         nextLevel();
       } else if (currentLevel === 2 && userEmotionAnswer.length === 9) {
+        $correct.hide();
         nextLevel();
+      } else {
+        console.log('wrong');
+        $welcomeMessage.hide();
+        $gameOutcome.html('Wrong answer! Have another go!');
+        $playAgain.html('Play Again?');
+        $playAgain.show();
+        $windows.hide();
+        $level2windows.hide();
+        $level.hide();
+        currentLevel = 1;
+        $($level).html(`Level ${currentLevel}`);
+        $main.addClass('oneWrong');
+        $playAgain.one('click', $playGameAgain);
       }
-    } else {
-      console.log('wrong');
-      $answerInstructions.hide();
-      $gameOutcome.html('Wrong answer! Have another go!');
-      $playAgain.html('Play Again?');
+    }
+  }
+
+  function nextLevel() {
+    $level.hide();
+    $welcomeMessage.hide();
+    $windows.hide();
+    $level2windows.hide();
+    $main.addClass('allCorrect');
+    $gameOutcome.html('Boom! You got them all correct!');
+
+    if (currentLevel === 1) {
       $playAgain.show();
-      $windows.hide();
-      $level2windows.hide();
-      $level.hide();
+      $playAgain.html('Play Level 2?');
+      setTimeout(function () {
+        currentLevel = 2;
+        $($level).html(`Level ${currentLevel}`);
+      }, 1000);
+      $playAgain.one('click', $playGameAgain);
+    } else {
+      $playAgain.show();
+      $playAgain.html('Play Again?');
       currentLevel = 1;
       $($level).html(`Level ${currentLevel}`);
-      $main.addClass('oneWrong');
+      $level.hide();
       $playAgain.one('click', $playGameAgain);
+      $level2windows.hide();
     }
-  });
-}
+  }
 
-function nextLevel() {
-  $level.hide();
-  $answerInstructions.hide();
-  $gameOutcome.html('Boom! You got them all correct!');
-  $windows.hide();
-  $level2windows.hide();
-  $main.addClass('allCorrect');
-
-  if (currentLevel === 1) {
-    $playAgain.show();
-    $playAgain.html('Play Level 2?');
-    setTimeout(function () {
-      currentLevel = 2;
-      $($level).html(`Level ${currentLevel}`);
-    }, 1000);
-    $playAgain.one('click', $playGameAgain);
-  } else {
-    $playAgain.show();
-    $playAgain.html('Play Again?');
-    currentLevel = 1;
-    $($level).html(`Level ${currentLevel}`);
-    $level.hide();
-    $playAgain.one('click', $playGameAgain);
+  function $playGameAgain() {
+    $($main).removeClass();
+    userClicks = 0;
+    $windows.off('click');
+    $windows.text('');
+    $gameOutcome.text('');
+    actualEmotionAnswer = [];
+    userEmotionAnswer = [];
+    emotions = ['Happy','Sad','Excited','Bored'];
+    emotionsL2 = ['Happy','Sad','Excited','Bored','Angry','Proud','Pensive','Tired','Hungry'];
+    $windows.show();
     $level2windows.hide();
-  }
-}
+    $level.show();
+    $playAgain.hide();
+    $welcomeMessage.show();
+    $welcomeMessage.html('Click on each box, try to remember the sequence!');
+    $windows.removeClass('selectEmotion');
 
-function $playGameAgain() {
-  $($main).removeClass();
-  userClicks = 0;
-  $windows.off('click');
-  $windows.text('');
-  $gameOutcome.text('');
-  actualEmotionAnswer = [];
-  userEmotionAnswer = [];
-  emotions = ['Happy','Sad','Excited','Bored'];
-  emotionsL2 = ['Happy','Sad','Excited','Bored','Angry','Proud','Pensive','Tired','Hungry'];
-  $windows.show();
-  $level2windows.hide();
-  $level.show();
-  $playAgain.hide();
-  $welcomeMessage.show();
-  $welcomeMessage.html('Click on each box to reveal an emotion! How many can you remember?');
-  $windows.removeClass('selectEmotion');
-
-  if (currentLevel === 2) {
-    $windows.css('width', `${400/3}px`);
-    $windows.css('height', `${400/3}px`);
-    $windows.removeClass('show-bg');
-    $level2windows.show();
+    if (currentLevel === 2) {
+      $windows.css('width', `${400/3}px`);
+      $windows.css('height', `${400/3}px`);
+      $windows.removeClass('show-bg');
+      $level2windows.show();
+    } else {
+      $windows.css('width', `${400/2}px`);
+      $windows.css('height', `${400/2}px`);
+      $windows.removeClass('show-bg');
+    }
+    setTimeout(() => {
+      assignClick();
+    }, 1000);
   }
-  else {
-    
-  }
-
-  setTimeout(() => {
-    assignClick();
-  }, 1000);
+});
 }
